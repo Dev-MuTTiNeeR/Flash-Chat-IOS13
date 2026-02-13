7230bb9ce330dd2a4badaa831ce67249f5bde5c6
Dev-MuTTiNeeR <cemakkaya1312@gmail.com>
2026-02-13 12:37:22 -0300

    Added firestore database and created test code.

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FirebaseCore
+import FirebaseFirestore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
+        
+        let db = Firestore.firestore()
         
         return true
     }
